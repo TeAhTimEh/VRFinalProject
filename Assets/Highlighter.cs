f815f22fda7b2cdf8545066a6b25f12b21c4e685
Pommier <kpommie2@illinois.edu>
2017-04-17 00:30:15 -0500

    first commit

---
+++ Assets/Highlighter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlighter : MonoBehaviour {
+
+    public bool hit = false;
+    public float time = 0.1f;
+    Renderer rend;
+    Shader outline;
+    Shader standard;
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        outline = Shader.Find("Outlined/Silhouetted Bumped Diffuse");
+        standard = Shader.Find("Standard");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(hit == true)
+        {
+            rend.material.shader = outline;
+            time -= Time.deltaTime;
+            if (time <= 0)
+                hit = false;
+        }
+        else
+            rend.material.shader = standard;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public Transform cam;
+    Vector3 dir;
+    Renderer rend;
+    LineRenderer lineRenderer;
+	// Use this for initialization
+	void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        dir = cam.forward;
+
+        Ray ray = new Ray(cam.transform.position, dir);
+        RaycastHit raycast;
+        Vector3 endPos = cam.transform.position + (200 * dir);
+
+        if (Physics.Raycast(ray, out raycast, 200))
+        {
+            endPos = raycast.point;
+            if (raycast.collider.tag == "Platform")
+            {
+                Highlighter h = raycast.transform.gameObject.GetComponent<Highlighter>();
+                h.time = 0.1f;
+                h.hit = true;
+
+            }
+        }
+        //lineRenderer.SetPosition(0, cam.transform.position);
+        //lineRenderer.SetPosition(1, endPos);
+    }
+
+}
